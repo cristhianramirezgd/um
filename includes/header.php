371f2b8af233bf750e6671008baf87271acefb4f
Cristhian Ramirez <cristhian.ramirez@universomascotas.co>
2018-07-25 15:42:14 -0500

    Estilo: Añade libreria de iconos font awesome

---
+++ includes/header.php
@@ -0,0 +1,70 @@
+<header class="header">
+	<div class="topbar border-top-color-orange border-bottom-color-orange">
+		<div class="container-um top-wrapper">
+			<div class="top-containers">
+				<ul class="list-inline-um seals-value">
+					<li class="seal"><a href=""><i class="fas fa-shipping-fast"></i> Envío a tu domicilio</a></li>
+					<li class="seal"><a href=""><i class="fas fa-certificate"></i> Aprovecha</a></li>
+					<li class="seal"><a href=""><i class="fas fa-truck"></i> Devoluciones grátis</a></li>
+				</ul>
+			</div>
+			<div class="top-containers">
+				<a href="tel:+57 111 11 11"><i class="fas fa-mobile-alt"></i> Tél.: (+57) 111 11 11.</a>
+			</div>
+			<div class="top-containers">
+
+				<ul class="list-inline-um margin-none-um padding-none-um user-um">
+					<li class="mobile-settings-menu"><i class="fas fa-cog"></i></li>
+					<li><a href=""><i class="fas fa-briefcase text-color-gray-dark"></i> Quiero vender</a></li>
+					<li><a href=""><i class="fas fa-user-plus text-color-gray-dark"></i> Registrarme</a></li>
+					<li><a href=""><i class="fas fa-user text-color-gray-dark"></i> Ingresar</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<script>
+	function modificarString()
+	    {
+	        console.log("lo llamo");
+	        var busqueda = document.getElementById("q").value;
+	        console.log("busqueda");
+	        var result = str.busqueda(/'s|s/g, "");
+	        return result;
+	    }
+	</script>
+	<div class="container-search">
+		<div class="logo-um"><a href="/" title="universomascotas.co"><img src="/images/universo-mascotas-logo-v2.svg" alt="logo universomascotas.co"></a></div>
+		<div class="search">
+			<form class="search-um" action="/buscador/" method="GET" onclick="return modificarString()">
+			   		<input type="search" name="q" placeholder="Encuentra un universo de productos y servicios para tu mascota">
+					<input class="btn-um btn-um-red" type="submit" value="Buscar">
+			</form>
+		</div>
+		<div class="checkout-nav"></div>
+	</div>
+	<nav class="menu">
+		<div class="link-menu">
+			<a href="">Categoría</a>
+			<div class="megamenu">
+				<div class="nav-department">Para perros</div>
+				<div class="nav-department">Para Gatos</div>
+				<div class="nav-department">Comunidad</div>
+				<div class="nav-category">
+					<ul>
+						<li><a href="">title</a></li>
+						<li><a href="">link-2</a></li>
+						<li><a href="">link-3</a></li>
+						<li><a href="">link-4</a></li>
+						<li><a href="">link-5</a></li>
+						<li><a href="">link-6</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="link-menu"><a href=""></a>Para Perros</div>
+		<div class="link-menu"><a href=""></a>Para Gatos</div>
+		<div class="link-menu"><a href=""></a>Marcas</div>
+		<div class="link-menu"><a href=""></a>Sale</div>
+		<div class="link-menu"><a href=""></a>Comunidad</div>
+	</nav>
+</header>
