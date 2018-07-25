a84c00c6959ab5b5f7897605a9059661648209e5
Cristhian Ramirez <cristhian.ramirez@universomascotas.co>
2018-07-25 15:46:01 -0500

    Estilo: añade cdn de libreria  para tooltips css

--- includes/head.php
+++ includes/head.php
@@ -57,3 +57,4 @@
     <!-- icons font awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
     <!-- tooltips css -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/balloon-css/0.5.0/balloon.min.css">
